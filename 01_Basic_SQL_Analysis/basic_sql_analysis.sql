2bcca0233c1c28fb7f05759b29e74598772f7d9b
sevinc-letifli <svnclatifw@gmail.com>
2026-07-22 15:34:44 +0400

    Add basic SQL analysis queries

--- 01_Basic_SQL_Analysis/basic_sql_analysis.sql
+++ 01_Basic_SQL_Analysis/basic_sql_analysis.sql
@@ -1,1 +1,99 @@
+-- =====================================================
+-- ANALYSIS 1
+-- Sual: Hazırda satışda olan ən yüksək qiymətli 10 məhsul hansılardır?
+-- =====================================================
 
+SELECT 
+	   ProductID,
+	   ProductName,
+	   UnitPrice,
+	   UnitsInStock,
+	   Discontinued
+FROM Products
+WHERE Discontinued = 0
+ORDER BY UnitPrice DESC
+LIMIT 10;
+
+
+-- =====================================================
+-- ANALYSIS 2
+-- Sual: Hazırda stockda müəyyən edilmiş limitdən aşağı olan
+-- və yenidən sifariş tələb edən məhsullar hansılardır?
+-- =====================================================
+
+SELECT
+      ProductID,
+      ProductName,
+      UnitsInStock,
+      ReorderLevel,
+      UnitsOnOrder,
+      Discontinued
+FROM Products
+WHERE UnitsInStock <= ReorderLevel AND Discontinued = 0 AND UnitsOnOrder = 0
+ORDER BY UnitsInStock ASC;
+
+
+-- =====================================================
+-- ANALYSIS 3
+-- Sual: Tələb olunan çatdırılma tarixindən gec göndərilən sifarişlər hansılardır?
+-- =====================================================
+
+SELECT 
+	  OrderID,
+	  CustomerID,
+	  EmployeeID,
+	  OrderDate,
+	  RequiredDate,
+	  ShippedDate
+FROM Orders
+WHERE RequiredDate < ShippedDate;
+
+
+-- =====================================================
+-- ANALYSIS 4
+-- Sual: Qiyməti yüksək və stok səviyyəsi kritik olan məhsullar hansılardır?
+-- =====================================================
+
+SELECT
+      ProductID,
+      ProductName,
+	    UnitPrice,
+      UnitsInStock,
+      ReorderLevel,
+      Discontinued
+FROM Products
+WHERE UnitsInStock <= ReorderLevel AND Discontinued = 0 
+ORDER BY UnitPrice DESC;
+
+
+-- =====================================================
+-- ANALYSIS 5
+-- Sual: Satışdan çıxarılan, amma hələ də stockda qalan məhsullar hansılardır?
+-- =====================================================
+
+SELECT
+    ProductID,
+    ProductName,
+    UnitsInStock,
+    UnitPrice,
+	  ReorderLevel,
+	  Discontinued
+FROM Products
+WHERE Discontinued = 1 AND UnitsInStock > 0
+ORDER BY UnitsInStock DESC;
+
+
+-- =====================================================
+-- ANALYSIS 6
+-- Sual: Göndərilmə vaxtı yaxınlaşan, amma hələ göndərilməyən sifarişlər hansılardır?
+-- =====================================================
+
+SELECT
+    OrderID,
+    CustomerID,
+    OrderDate,
+    RequiredDate,
+    ShippedDate
+FROM Orders
+WHERE ShippedDate IS NULL
+ORDER BY RequiredDate ASC;
